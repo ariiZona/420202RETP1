fa6354a6bbee0056c90c2af8edc5294a0a1f8cde
arizona <hublo11@hotmail.fr>
2018-09-17 14:31:07 -0400

    plotxd

--- 420202RETP1/Position.h
+++ 420202RETP1/Position.h
@@ -1,2 +1,15 @@
 #pragma once
 
+template <typename T>
+class Position {
+private:
+	int startX, startY, endX, endY; // Coordonnées d'entrées et de sorties
+	
+
+public:
+	Position() {
+		startX = startY = endX = endY = 0;
+
+	}
+
+};
